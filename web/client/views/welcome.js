f6af8207496597e774e6593358d322eefa3850bb
Dmitriy Melnik <d.b.melnik@yandex.ru>
2015-09-28 15:02:39 +0600

    Реализация базовой разметки главной страницы

--- web/client/views/welcome.js
+++ web/client/views/welcome.js
@@ -10,28 +10,9 @@ export default class Welcome extends Component {
 
   render () {
     return <div className="l-layout p-welcome">
-      <div className="p-w-top"></div>
-      <div className="p-w-body">
-        <div className="p-w-container">
-          <section>
-            <div className="m-logo m-logo-large">
-              <h1 className="m-l-title">{this.lang.brand.name}</h1>
-              <h2 className="m-l-tagline">{this.lang.brand.tagline}</h2>
-            </div>
-          </section>
-          <section>
-            <h4>Новости</h4>
-            <p>
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-            </p>
-            <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
-            </p>
-          </section>
-        </div>
-      </div>
-      <div className="p-w-bottom"></div>
-      <Footer />
+      <div className="l-sidebar">1</div>
+      <div className="l-content">2</div>
+      <div className="l-footer">3</div>
     </div>;
   }
 }
