0810e1e8d309ae6e4ebb326ec2761c43ff6488b9
Dmitriy Melnik <d.b.melnik@yandex.ru>
2015-10-05 16:32:31 +0600

    Реализация страницы входа на materialize

--- web/client/views/signin.js
+++ web/client/views/signin.js
@@ -22,22 +22,19 @@ export default class Signin extends Form {
     return <div className='l-layout p-signin'>
       <Navbar/>
       <div className='l-content'>
-        <div className='p-s-form'>
-          <form onSubmit={this.handleSubmit}>
-            <div className='m-control-list'>
-              <div className='m-control-group'>
-                <span className='m-cg-icon icon-user'></span>
-                <input valueLink={this.linkState('model.username')} type='text' className='m-control' placeholder={this.lang.fields.username} required />
-              </div>
-              <div className='m-control-group'>
-                <span className='m-cg-icon icon-lock'></span>
-                <input valueLink={this.linkState('model.password')} type='password' className='m-control' placeholder={this.lang.fields.password} required />
-              </div>
-            </div>
-            <p className='l-text-center'>
-              <button type='submit' className='m-btn'>{this.lang.captions.signin}</button>
-            </p>
-          </form>
+        <h1 className='cyan-text text-darken-3'>Вход</h1>
+        <div className='row'>
+          <div className='p-s-form card-panel col s4'>
+            <form onSubmit={this.handleSubmit}>
+              <input valueLink={this.linkState('model.username')} type='text' placeholder={this.lang.fields.username} required />
+              <input valueLink={this.linkState('model.password')} type='password' placeholder={this.lang.fields.password} required />
+              <p className='l-text-center'>
+                <button className="btn waves-effect waves-light" type='submit'>
+                  {this.lang.captions.signin}
+                </button>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
     </div>
